9efe9b12f67449c97a095be8b3960e81e029de20
Goose Lord <thegooselord@protonmail.com>
2022-10-05 06:50:12 +0100

    working

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 import os
+import re
+import socket
+import requests
+import json
+from dotenv import load_dotenv
 import discord
 
+load_dotenv()
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+def update_dns(ip, port):
+    recordA = pbConfig.copy()
+    recordA.update({"content": ip, "ttl": "30" })
+
+    endpointA = recordA["endpoint"] + '/dns/editByNameType/' + os.getenv("DOMAIN") + "/A/" + os.getenv("SUBDOMAIN")
+
+    recordSRV = pbConfig.copy()
+    recordSRV.update({ "content": "0 " + port + " " + os.getenv("SUBDOMAIN") + "." + os.getenv("DOMAIN"), "ttl": "30" })
+    
+    endpointSRV = recordSRV["endpoint"] + '/dns/editByNameType/' + os.getenv("DOMAIN") + "/SRV/_minecraft._tcp." + os.getenv("SUBDOMAIN")
+
+    print(recordA)
+    print(recordSRV)
+    print(endpointA)
+    print(endpointSRV)
+
+    resultA = json.loads(requests.post(endpointA, data = json.dumps(recordA)).text)
+    print("A   - " + resultA["status"])
+
+    resultSRV = json.loads(requests.post(endpointSRV, data = json.dumps(recordSRV)).text)
+    print("SRV - " + resultSRV["status"])
+
+    print(resultA)
+    print(resultSRV)
+
+    
+    return
+
 
 @client.event
 async def on_ready():
@@ -13,11 +48,32 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    print(message.content)
+    if message.author.display_name == str(os.getenv('NGROK_PROVIDER')):
+        ip_port = re.search(r"is *([^\n\r ]*)", message.content).group(1)
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        for i in range(len(ip_port) - 1, 0, -1):
+            if ip_port[i] == ":":
+                port = ip_port[i+1:]
+                colon = i
+                break
+
+            if i == 0:
+                await message.channel.send("Error: IP formatted incorrectly")
+                return
+
+        host = ip_port[:i]
+        ip = socket.gethostbyname(host)
+
+        await message.channel.send(ip + ":" + port + " (" + host + ")")
+
+        update_dns(ip, port)
+
+    return
+
+
+pbConfig = json.loads('{ "endpoint":"https://api-ipv4.porkbun.com/api/json/v3", "apikey": "%s", "secretapikey": "%s" }' % (os.getenv('PORKBUN_API'), os.getenv('PORKBUN_SECRET')))
+
 
 client.run(os.getenv('DISCORD_TOKEN'))
 
